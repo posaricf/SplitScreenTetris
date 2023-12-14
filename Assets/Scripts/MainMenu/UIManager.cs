9e60531be8b7180ea5847e116bee10eef7219304
Fran Posarić <fposaric@gmail.com>
2023-12-14 18:05:27 +0100

    Changes to some imports

--- Assets/Scripts/MainMenu/UIManager.cs
+++ Assets/Scripts/MainMenu/UIManager.cs
@@ -1,8 +1,3 @@
-using GameScene;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
